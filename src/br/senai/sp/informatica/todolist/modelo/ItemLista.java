07ba7920e0484d057308d344d63a9d3dc817bac8
Rafa00el00 <whitestripes@gmail.com>
2016-09-17 09:28:42 -0300

    subindo o Projeto

---
+++ src/br/senai/sp/informatica/todolist/modelo/ItemLista.java
@@ -0,0 +1,53 @@
+package br.senai.sp.informatica.todolist.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ItemLista {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String descricao;
+	private boolean feito;
+	@ManyToOne
+	@JoinColumn(name="lista_id")
+	private Lista lista;
+
+	public Lista getLista() {
+		return lista;
+	}
+
+	public void setLista(Lista lista) {
+		this.lista = lista;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public boolean isFeito() {
+		return feito;
+	}
+
+	public void setFeito(boolean feito) {
+		this.feito = feito;
+	}
+
+}
